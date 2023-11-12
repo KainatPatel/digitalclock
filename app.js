bbb66d0af8f04a84d7fc4d8263c89a54d6f36656
KainatPatel <150426335+KainatPatel@users.noreply.github.com>
2023-11-12 18:25:24 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+/*function dislayTime(){
+    var d = new Date();
+    var hour = d.getHours();
+    var min = d.getMinutes();
+    var sec = d.getSeconds();
+    document.getElementById("clock").innerHTML = hour + ":" + min + ":" + sec ;
+
+}
+setInterval(dislayTime,1000)*/
